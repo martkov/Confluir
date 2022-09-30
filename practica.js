dab3a86c3355136dda44f1a995944fbffe2d67c7
martkov <martkov@gmail.com>
2022-09-30 10:47:00 -0300

    primera version

---
+++ practica.js
@@ -0,0 +1,17 @@
+function esPar(num) {
+    if (num % 2 == 0)
+        es = true;
+    else
+        es = false;
+    return es
+    
+}
+console.log(esPar(20))
+
+
+
+let suma=0;
+for(var i=0;i<=5;i++){
+  suma=suma+i;
+}
+console.log(i)
